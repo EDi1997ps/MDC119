4d5d6d9bbf08c4ae9be81369acac7b3ebc52a226
unknown <1922082019@academico.iesb.br>
2020-02-12 10:44:20 -0300

    adicionando arquivo ola mundo

---
+++ Aulas/aulas01/olamundo.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char const *argv[]){
+printf("asdfhasdfj:");
+return 0;
+}//asdasdasdasdasdasd
+//dsadasdasdasdasdasdasdas
+/*dsadsadasdasdasjaskfakglahdfgjksdaflghsdafljsdakfjsdklakfjasdkjf
+
+
