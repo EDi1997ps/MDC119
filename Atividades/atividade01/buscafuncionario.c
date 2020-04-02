c6e3b087658b60f9c6eef1646853e79da7e2193f
Edinilson1922082019 <60969430+Edinilson1922082019@users.noreply.github.com>
2020-04-02 09:01:09 -0300

    Update buscafuncionario.c

--- Atividades/atividade01/buscafuncionario.c
+++ Atividades/atividade01/buscafuncionario.c
@@ -4,7 +4,7 @@
 int main() {
 	system("color fC");
     int funcionario[][0]={{1111,30001000},{2222,30002000},{3333,30003000},{4444,30004000},{5555,30005000},{6666,30006000},{7777,30007000}};
-    //só aparece algo se colocar o [0], se colocar por exemplo o [10] e [1] nao aparece nada.
+    //sÃ³ aparece algo se colocar o [0], se colocar por exemplo o [0][10] e [0][1] nao aparece nada.
     int matricula, a;
     scanf("%d", &matricula);
     for(a=0; a<7; a++) {
